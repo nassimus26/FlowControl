d1df60850f76f642e8d64ca9d8ea0ca822edcf4a
Nassim MOUALEK <cd_boite@yahoo.fr>
2017-09-03 18:46:05 +0200

    first

--- README.md
+++ README.md
@@ -8,26 +8,28 @@ Main classes :
 
 **BufferedFlowContorlExecutor** exemple :
 ```java
-@Test
+    @Test
     public void testFlowControl() throws InterruptedException {
         AtomicInteger count = new AtomicInteger();
-        int size = 1000000;
+        int size = 1_000_000;
         final List<String> values = new ArrayList<>();
-        for (int i=0;i<size;i++) {
+        for (int i=0;i<size;i++)
             values.add(transformRow(generateRow(i)));
-        }
+
         final List<String> result = new Vector<>();
-        BufferedFlowControlExecutor<String> processRows = new BufferedFlowControlExecutor<String>(new BuffredCallable<String>() {
-                @Override
-                public void call(Object[] values) throws Throwable {
-                    for (Object s: values)
-                        result.add( transformRow((String)s) );
-                }
-            }, 100, 4, 20, "processRows") {
-            @Override
-            public boolean isWorkDone() {
-                return count.get()==size;
-            }
+        BufferedFlowControlExecutor<String> processRows =
+                new BufferedFlowControlExecutor<String>(
+                    new BuffredCallable<String>() {
+                        @Override
+                        public void call(Object[] values) throws Throwable {
+                            for (Object s: values)
+                                result.add( transformRow((String)s) );
+                        }
+                    }, 100, BufferedFlowControlExecutor.getNbCores(), 20, "processRows") {
+                        @Override
+                        public boolean isWorkDone() {
+                            return count.get()==size;
+                        }
         };
         while (count.get()<size){
             try {
@@ -42,6 +44,14 @@ Main classes :
         Collections.sort(values);
         Collections.sort(result);
         Assert.assertArrayEquals( values.toArray(), result.toArray());
+    }
+    private String generateRow(int i){
+        return "row_"+i;
+    }
+
+    private String transformRow(String row){// some CPU operations
+        return row.replaceAll("row", "replaceMe").replaceAll("_", " ")
+                .replaceAll("replaceMe", "ligne");
     }
 
 ```
--- src/test/java/org/flowcontrol/BufferedFlowControlExecutorTest.java
+++ src/test/java/org/flowcontrol/BufferedFlowControlExecutorTest.java
@@ -13,34 +13,28 @@ import java.util.concurrent.atomic.AtomicInteger;
 * */
 public class BufferedFlowControlExecutorTest {
 
-    private String generateRow(int i){
-        return "row_"+i;
-    }
-
-    private String transformRow(String row){
-        return row.replaceAll("row", "ligne").replaceAll("_", " ");
-    }
-
     @Test
     public void testFlowControl() throws InterruptedException {
         AtomicInteger count = new AtomicInteger();
-        int size = 1000000;
+        int size = 1_000_000;
         final List<String> values = new ArrayList<>();
-        for (int i=0;i<size;i++) {
+        for (int i=0;i<size;i++)
             values.add(transformRow(generateRow(i)));
-        }
+
         final List<String> result = new Vector<>();
-        BufferedFlowControlExecutor<String> processRows = new BufferedFlowControlExecutor<String>(new BuffredCallable<String>() {
-                @Override
-                public void call(Object[] values) throws Throwable {
-                    for (Object s: values)
-                        result.add( transformRow((String)s) );
-                }
-            }, 100, 4, 20, "processRows") {
-            @Override
-            public boolean isWorkDone() {
-                return count.get()==size;
-            }
+        BufferedFlowControlExecutor<String> processRows =
+                new BufferedFlowControlExecutor<String>(
+                    new BuffredCallable<String>() {
+                        @Override
+                        public void call(Object[] values) throws Throwable {
+                            for (Object s: values)
+                                result.add( transformRow((String)s) );
+                        }
+                    }, 100, BufferedFlowControlExecutor.getNbCores(), 20, "processRows") {
+                        @Override
+                        public boolean isWorkDone() {
+                            return count.get()==size;
+                        }
         };
         while (count.get()<size){
             try {
@@ -55,6 +49,14 @@ public class BufferedFlowControlExecutorTest {
         Collections.sort(values);
         Collections.sort(result);
         Assert.assertArrayEquals( values.toArray(), result.toArray());
+    }
+    private String generateRow(int i){
+        return "row_"+i;
+    }
+
+    private String transformRow(String row){// some CPU operations
+        return row.replaceAll("row", "replaceMe").replaceAll("_", " ")
+                .replaceAll("replaceMe", "ligne");
     }
 
 }

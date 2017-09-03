7fc14ad1cc2aa1f516756bae920781dc73a40237
Nassim MOUALEK <cd_boite@yahoo.fr>
2017-09-03 18:27:48 +0200

    first

---
+++ src/test/java/org/flowcontrol/BufferedFlowControlExecutorTest.java
@@ -0,0 +1,58 @@
+package org.flowcontrol;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.nassimus.thread.BufferedFlowControlExecutor;
+import org.nassimus.thread.BuffredCallable;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BufferedFlowControlExecutorTest {
+
+    private String generateRow(int i){
+        return "row_"+i;
+    }
+
+    private String transformRow(String row){
+        return row.replaceAll("row", "ligne").replaceAll("_", " ");
+    }
+
+    @Test
+    public void testFlowControl() throws InterruptedException {
+        AtomicInteger count = new AtomicInteger();
+        int size = 1000000;
+        final List<String> values = new ArrayList<>();
+        for (int i=0;i<size;i++) {
+            values.add(transformRow(generateRow(i)));
+        }
+        final List<String> result = new Vector<>();
+        BufferedFlowControlExecutor<String> processRows = new BufferedFlowControlExecutor<String>(new BuffredCallable<String>() {
+                @Override
+                public void call(Object[] values) throws Throwable {
+                    for (Object s: values)
+                        result.add( transformRow((String)s) );
+                }
+            }, 100, 4, 20, "processRows") {
+            @Override
+            public boolean isWorkDone() {
+                return count.get()==size;
+            }
+        };
+
+        while (count.get()<size){
+            try {
+                processRows.submit("row_"+count.get());
+                count.incrementAndGet();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("waiting");
+        processRows.waitAndFlushAndShutDown();
+        Collections.sort(values);
+        Collections.sort(result);
+        Assert.assertArrayEquals( values.toArray(), result.toArray());
+    }
+
+}

ee6e1fab4c46e4fc62e1dd61c3cbdf291a069802
Nassim MOUALEK <cd_boite@yahoo.fr>
2017-09-14 11:51:24 +0200

    pom

---
+++ src/main/java/org/nassimus/thread/util/SimpleObjectPool.java
@@ -0,0 +1,38 @@
+package org.nassimus.thread.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class SimpleObjectPool<T> {
+    private final int maxNbObjects;
+    private final Map<T, Boolean> objects;
+    private T[] objectsArray;
+    public SimpleObjectPool(int maxNbObjects){
+        this.maxNbObjects = maxNbObjects;
+        objects = new HashMap<>(maxNbObjects);
+    }
+
+    public abstract T createAnObject();
+
+    public T checkOut() {
+        if (objectsArray!=null)
+            for (T object : objectsArray) {
+                Boolean state = objects.get(object);
+                if (state!=null && !state) {
+                    objects.put(object, true);
+                    return object;
+                }
+            }
+        T object = createAnObject();
+        objects.put(object, true);
+        synchronized (this) {
+            objectsArray = (T[]) objects.keySet().toArray();
+        }
+        return object;
+    }
+    public void release(T object) {
+        synchronized (this){
+            objects.put(object, false);
+        };
+    }
+}
